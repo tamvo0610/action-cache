117a688df5b259cc04452e70206a1e85b2507997
tamvo0610 <tamvo0610@gmail.com>
2024-01-26 11:04:57 +0700

    Update Code

--- src/save.ts
+++ src/save.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import { mkdirP, mv, cp, rmRF } from '@actions/io'
+import * as io from '@actions/io'
 import { exists } from '@actions/io/lib/io-util'
 import { getVars, isErrorLike } from './utils/actionUtils'
 import * as fs from 'fs'
@@ -9,13 +9,19 @@ import { execSync, exec } from 'child_process'
 
 async function save() {
   try {
-    const { cachePath, targetPath } = getVars()
+    const { cacheDir, cachePath, targetPath } = getVars()
     const isCacheExist = await exists(cachePath)
     if (isCacheExist) return
-    execSync(`mkdir -p ${cachePath}`)
-    Log.info(`Create Cache Folder ${cachePath}`)
-    execSync(`rsync -a ${targetPath}/ ${cachePath}`)
-    Log.info(`Sync Cache Folder ${targetPath} to ${cachePath}`)
+    await io.mkdirP(cacheDir)
+    await io.rmRF(cachePath)
+    await io.cp(targetPath, cachePath, {
+      copySourceDirectory: true,
+      recursive: true
+    })
+    // execSync(`mkdir -p ${cachePath}`)
+    // Log.info(`Create Cache Folder ${cachePath}`)
+    // execSync(`rsync -a ${targetPath}/ ${cachePath}`)
+    // Log.info(`Sync Cache Folder ${targetPath} to ${cachePath}`)
   } catch (error: any) {
     const errorMessage = isErrorLike(error) ? error.message : error
     core.setFailed(Log.error(errorMessage))
