0594d474efdd8d64de29d734e4850a110e1af501
tamvo0610 <tamvo0610@gmail.com>
2024-02-01 09:40:39 +0700

    Update Code

--- src/restore.ts
+++ src/restore.ts
@@ -19,21 +19,20 @@ async function restore() {
       await runExec(`rsync -a ${cachePath}/ ${targetDir}`)
       Log.info('Cache restore success')
       return core.setOutput('cache-hit', true)
-    } else {
-      Log.info('Cache not exist, skip restore')
-      console.log('Action: ', options?.action)
-      if (!!options?.action) {
-        Log.info('Action cache provided, run action')
-        await runExec(`cd ${options.workingDir} && ${options.action}`)
-        Log.info('Create cache folder')
-        await runExec(`mkdir -p ${cachePath}`)
-        Log.info('Sync cache folder')
-        await runExec(`rsync -a ${targetPath}/ ${cachePath}`)
-        Log.info('Cache save success')
-        return core.setOutput('cache-hit', true)
-      }
-      core.setOutput('cache-hit', false)
     }
+    Log.info('Action: ' + options?.action)
+    Log.info('Cache not exist, skip restore')
+    if (!!options?.action) {
+      Log.info('Action cache provided, run action')
+      await runExec(`cd ${options.workingDir} && ${options.action}`)
+      Log.info('Create cache folder')
+      await runExec(`mkdir -p ${cachePath}`)
+      Log.info('Sync cache folder')
+      await runExec(`rsync -a ${targetPath}/ ${cachePath}`)
+      Log.info('Cache save success')
+      return core.setOutput('cache-hit', true)
+    }
+    core.setOutput('cache-hit', false)
   } catch (error: any) {
     const errorMessage = isErrorLike(error) ? error.message : error
     core.setFailed(Log.error(errorMessage))
