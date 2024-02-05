8fdfb0b05825afaed820f99b2c967d5f005ae9dd
tamvo0610 <tamvo0610@gmail.com>
2024-02-05 16:36:32 +0700

    Update Code

--- src/save.ts
+++ src/save.ts
@@ -19,7 +19,7 @@ export async function save() {
     // await io.mv(targetPath, cachePath, {
     //   force: true
     // })
-    await runExec(`rsync -av ${targetPath} ${cachePath}`)
+    await runExec(`rsync -a ${targetPath} ${cachePath}`)
     Log.info('Cache save success')
   } catch (error: any) {
     const errorMessage = isErrorLike(error) ? error.message : error
